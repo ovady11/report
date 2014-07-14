9f2aac9d3050eaea3db2f5ad1ad95543e0e28717
Avi <Avi@admintheweb.com>
2014-07-14 15:36:30 +0300

    Update version for Rupin

--- version.php
+++ version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2014070901;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012120310;        // Requires this Moodle version
+$plugin->requires  = 2012120305;        // Requires this Moodle version
 $plugin->component = 'report_teacherreport'; // Full name of the plugin (used for diagnostics)
